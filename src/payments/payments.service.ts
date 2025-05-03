fb8695939cfd7e95594d91870962befc75cf3738
José Alonso <mail@josealonso.dev>
2025-05-03 02:21:37 -0600

    Create payment session

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,30 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      // TODO: Colocar aqui el ID de mi ordern
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-Shirt',
+            },
+            unit_amount: 2000, // 20 dollars, 2000/100 = 20.00
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3006/payments/success',
+      cancel_url: 'http://localhost:3006/payments/cancel',
+    });
+
+    return session;
+  }
 }
