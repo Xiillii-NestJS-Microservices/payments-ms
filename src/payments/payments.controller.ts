17fe18b9908fedcf54a4d0c37b5fd6d9974dd9d7
José Alonso <mail@josealonso.dev>
2025-05-05 13:12:26 -0600

    Refactor microservice connection and update payment session handling

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Logger, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('payments')
 export class PaymentsController {
@@ -10,8 +11,12 @@ export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('create-payment-session')
+  @MessagePattern('create.payment.session')
   createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
-    return this.paymentsService.createPaymentSession(paymentSessionDto);
+    return {
+      paymentSessionDto,
+    };
+    // return this.paymentsService.createPaymentSession(paymentSessionDto);
   }
 
   @Get('success')
